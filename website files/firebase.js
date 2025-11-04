68cd26d89c1d1d98e5b5c8861884e73dc2c1f044
Dylan Berry <dylanjberry@icloud.com>
2025-11-03 19:49:15 -0800

    added the website files

---
+++ website files/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
