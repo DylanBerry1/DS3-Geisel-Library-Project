db02d0fe2d85579b2d92ea7f55cfa21cc5d0de5d
Dylan Berry <dylanjberry@icloud.com>
2025-11-11 00:35:32 -0800

    fix to blank page issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DS3-Geisel-Library-Project"
+  base: "/DS3-Geisel-Library-Project/"
 })
